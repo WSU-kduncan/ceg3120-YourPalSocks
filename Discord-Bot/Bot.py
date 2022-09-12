712bc160bc16a46b3052add6def4e8b14a3f6146
YourPalSocks <ethanMH2002@gmail.com>
2022-09-12 11:07:20 -0400

    Bot build complete

--- Discord-Bot/Bot.py
+++ Discord-Bot/Bot.py
@@ -5,13 +5,14 @@ import os
 import discord
 import random
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
-#print(os.getenv('DISCORD_TOKEN'))
+# print(os.getenv('DISCORD_TOKEN'))
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+client = discord.Client(intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
@@ -25,31 +26,58 @@ async def on_ready():
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
+    # Seed so it tells different jokes on different instances
+    random.seed(datetime.now)
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == client.user: # Don't recognize your own messages
         return
 
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
+    # Message and image arrays
+    joke_prompts = [
+        'I heard two peanuts were walking down the street...',
+        'If April showers bring May flowers, what do May flowers bring?',
+        'What do you call a factory that makes okay products?',
+        'What did the ocean say to the beach?'
+    ]
+    punchlines = [
+        'One was a-salted.',
+        'Pilgrims',
+        'A satisfactory',
+        'Nothing, it just waved'
+    ]
+    reaction_imgs = [
+        'https://c.tenor.com/J9l-6hIfBXYAAAAC/my-honest-reaction-my-honest-reaction-meme.gif',
+        'https://c.tenor.com/nON19NSBKXsAAAAC/my-honest-reaction.gif',
+        'https://i.pinimg.com/564x/cd/88/ee/cd88ee0db9948b8247785502a75fe2f1.jpg',
+        'https://c.tenor.com/D7krIUXuUAYAAAAd/my-honest-reaction-my-honest-reaction-meme.gif'
+
     ]
 
-    hitchhiker_quotes = [
-        'There is an art, it says, or rather, a knack to flying. The knack lies in learning how to throw yourself at the ground and miss.',
-        'It is a mistake to think you can solve any major problems just with potatoes.',
-        'In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.',
-        'A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.',
-    ]
-
-    if message.content == 'towel!':
-        #response = random.choice(brooklyn_99_quotes)
-        response = random.choice(hitchhiker_quotes)
+    if message.content == "!joke": # !joke- Send two messages: the joke prompt and the punchline via parallel arrays
+        resp_id = random.randint(0, joke_prompts.__len__() - 1)
+        # Send prompt
+        response = joke_prompts[resp_id]
         await message.channel.send(response)
+        # Send punchline
+        response = punchlines[resp_id]
+        await message.channel.send(response)
+
+    if message.content == "!myreaction": # Get Rowdy's honest reaction
+        react_id = random.randint(0, reaction_imgs.__len__() - 1)
+        # Send image
+        await message.channel.send(reaction_imgs[react_id])
+
+    if message.content == "!help": # Print command info
+        await message.channel.send("!joke -- I'll tell you a joke")
+        await message.channel.send("!myreaction -- Get my honest reaction")
+        await message.channel.send("!kill -- Close bot instance")
+
+
+    if message.content == "!kill": # TESTING ONLY: close bot client (gives me console controls back)
+        await message.channel.send("Bye bye!")
+        await client.close()
+    
 
 client.run(TOKEN)
